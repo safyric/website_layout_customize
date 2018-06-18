e05ec9f0ee003a1e34de109f4ae8680160eaa7ff
jchanme <39323414+jchanme@users.noreply.github.com>
2018-06-18 13:45:10 +0800

    Update res_company.py

--- models/res_company.py
+++ models/res_company.py
@@ -3,6 +3,6 @@
 from odoo import models, fields
 
 class ResCompany(models.Model):
-	_inherit = 'res.company'
+    _inherit = 'res.company'
 
-	report_header_name = fields.Text(translate=True)
+    report_header_name = fields.Text(translate=True)
